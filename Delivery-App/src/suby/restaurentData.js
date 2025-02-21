1363ca615de0189787564034c768f68b68fbc86d
Ritu Koley <100666808+RituKoley@users.noreply.github.com>
2025-02-21 21:35:49 +0530

    Add files via upload

---
+++ Delivery-App/src/suby/restaurentData.js
@@ -0,0 +1,7 @@
+// export const restaurantData = [
+//     { item_img: "/assets/restaurants/Restaurant1.jpg" },
+//     { item_img: "/assets/restaurants/Restaurant2.jpg" },
+//     { item_img: "/assets/restaurants/Restaurant3.jpg" },
+//     { item_img: "/assets/restaurants/Restaurant4.jpg" },
+//     { item_img: "/assets/restaurants/Restaurant5.jpg" }
+// ];
